3dbc3929dcac4783213d84da5582dc8b8ef5889b
Elaine <elaine85leal@gmail.com>
2025-06-21 18:28:47 -0500

    Subiendo cambios funcionales inicio

--- Frontend/src/app/pages/Inicio/inicio.ts
+++ Frontend/src/app/pages/Inicio/inicio.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  standalone: true,
   templateUrl: './inicio.html',
-  styleUrl: './inicio.css'
+  styleUrls: ['./inicio.css']
 })
 export class Inicio {
-  
+  scrollToSection(id: string): void {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
