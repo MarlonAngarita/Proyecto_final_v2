5646cc4a26ea1f14d20dc04e98549d1e78327bce
Elaine <elaine85leal@gmail.com>
2025-06-21 20:17:18 -0500

    subiendo funcionalidad boton al login

--- Frontend/src/app/pages/Inicio/inicio.ts
+++ Frontend/src/app/pages/Inicio/inicio.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
   templateUrl: './inicio.html',
-  styleUrls: ['./inicio.css']
+  styleUrls: ['./inicio.css'],
+  imports: [CommonModule, RouterModule, FormsModule]
 })
 export class Inicio {
+  mostrarConfirmacion = false;
+
   scrollToSection(id: string): void {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
+    const el = document.getElementById(id);
+    if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  onSubmit(form: NgForm): void {
+    this.mostrarConfirmacion = true;
+    form.resetForm();
+
+    setTimeout(() => {
+      this.mostrarConfirmacion = false;
+    }, 5000);
+  }
+
+  cerrarModal(): void {
+    this.mostrarConfirmacion = false;
   }
 }
