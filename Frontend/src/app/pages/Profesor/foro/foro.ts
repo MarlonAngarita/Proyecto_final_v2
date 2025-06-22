b639cbe6681e46e4fb773b8ea1aba4b2ae7a91b1
alejandracaicedoo <heidy_acaicedo@soy.sena.edu.co>
2025-06-22 16:13:04 -0500

    subiendo cambios en foro y logos en opacidad

--- Frontend/src/app/pages/Profesor/foro/foro.ts
+++ Frontend/src/app/pages/Profesor/foro/foro.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-foro',
-  imports: [],
+  selector: 'app-foro-profesores',
+  imports: [CommonModule, FormsModule],
+  standalone: true,
   templateUrl: './foro.html',
-  styleUrl: './foro.css'
+  styleUrl: './foro.css',
 })
 export class Foro {
+  tituloPost: string = '';
+  contenidoPost: string = '';
+  publicaciones: { autor: string; tiempo: string; contenido: string; imagen: string }[] = [
+    {
+      autor: 'Usuario 1',
+      tiempo: 'Hace 5 min',
+      contenido: 'Este foro es increíble, ¿qué opinan?',
+      imagen:
+        'https://img.freepik.com/vector-premium/entrenador-fitness-imagen-vectorial-icono-femenino-puede-usar-profesiones_120816-263153.jpg?w=740',
+    },
+  ];
 
+  publicarPost() {
+    if (this.tituloPost.trim() && this.contenidoPost.trim()) {
+      this.publicaciones.unshift({
+        autor: 'Profesor',
+        tiempo: 'Hace un momento',
+        contenido: this.contenidoPost,
+        imagen:
+          'https://img.freepik.com/vector-premium/entrenador-fitness-imagen-vectorial-icono-femenino-puede-usar-profesiones_120816-263153.jpg?w=740',
+      });
+
+      this.tituloPost = '';
+      this.contenidoPost = '';
+    }
+  }
+
+  eliminarPost(index: number) {
+    this.publicaciones.splice(index, 1);
+  }
 }
