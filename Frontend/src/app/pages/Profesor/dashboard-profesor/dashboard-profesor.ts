58414ec6069ef42054362f03dc7a6f5af1de7de2
Elaine <elaine85leal@gmail.com>
2025-06-22 13:36:32 -0500

    subiendo carpeta rol profesor

---
+++ Frontend/src/app/pages/Profesor/dashboard-profesor/dashboard-profesor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-profesor',
+  imports: [],
+  templateUrl: './dashboard-profesor.html',
+  styleUrl: './dashboard-profesor.css'
+})
+export class DashboardProfesor {
+
+}
