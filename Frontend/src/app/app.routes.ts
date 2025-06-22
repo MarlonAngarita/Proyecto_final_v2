d81db7e0b226e5444eae469e9709c3b9b6d364e8
Elaine <elaine85leal@gmail.com>
2025-06-22 14:36:14 -0500

    actualizacion rutas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/Inicio/inicio';
 import { Login } from './pages/Login/login';
 import { Registro } from './pages/Registro/registro';
+import { Cursos } from './pages/Profesor/cursos/cursos';
+import { DashboardProfesor } from './pages/Profesor/dashboard-profesor/dashboard-profesor';
+import { Desafios } from './pages/Profesor/desafios/desafios';
+import { Foro } from './pages/Profesor/foro/foro';
+import { Modulos } from './pages/Profesor/modulos/modulos';
 
 export const routes: Routes = [
   { path: '', component: Inicio },
   { path: 'login', component: Login },
   { path: 'registro', component: Registro },
+  { path: 'cursos', component: Cursos },
+  { path: 'dashboard-profesor', component: DashboardProfesor },
+  { path: 'desafios', component: Desafios },
+  { path: 'foro', component: Foro },
+  { path: 'modulos', component: Modulos },
 ];
