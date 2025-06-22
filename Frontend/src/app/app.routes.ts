cffd1f8fe41ef7cde6b28fd1bc8934b128da6f16
Elaine <elaine85leal@gmail.com>
2025-06-22 15:30:50 -0500

    actualizacion dashboard-profesor

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,19 +2,23 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/Inicio/inicio';
 import { Login } from './pages/Login/login';
 import { Registro } from './pages/Registro/registro';
+// Importar las páginas del profesor
 import { Cursos } from './pages/Profesor/cursos/cursos';
 import { DashboardProfesor } from './pages/Profesor/dashboard-profesor/dashboard-profesor';
 import { Desafios } from './pages/Profesor/desafios/desafios';
 import { Foro } from './pages/Profesor/foro/foro';
 import { Modulos } from './pages/Profesor/modulos/modulos';
+// Importar las páginas del usuario 
 
 export const routes: Routes = [
   { path: '', component: Inicio },
   { path: 'login', component: Login },
   { path: 'registro', component: Registro },
-  { path: 'cursos', component: Cursos },
-  { path: 'dashboard-profesor', component: DashboardProfesor },
-  { path: 'desafios', component: Desafios },
-  { path: 'foro', component: Foro },
-  { path: 'modulos', component: Modulos },
+  // Rutas del profesor
+  { path: 'profesor/cursos', component: Cursos },
+  { path: 'profesor/dashboard-profesor', component: DashboardProfesor },
+  { path: 'profesor/desafios', component: Desafios },
+  { path: 'profesor/foro', component: Foro },
+  { path: 'profesor/modulos', component: Modulos },
+  // Rutas del usuario
 ];
--- Frontend/src/app/pages/Profesor/dashboard-profesor/dashboard-profesor.ts
+++ Frontend/src/app/pages/Profesor/dashboard-profesor/dashboard-profesor.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-profesor',
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './dashboard-profesor.html',
   styleUrl: './dashboard-profesor.css'
 })
 export class DashboardProfesor {
+  nombreProfesor = 'Profe Sandra';
+avatarURL = '/assets/avatar-profesor.png';
+
+cursosAsignados = [
+  { id: 1, nombre: 'Lógica Computacional', estudiantes: ['Carlos', 'Ana', 'Luis'] },
+  { id: 2, nombre: 'Pensamiento Algorítmico', estudiantes: ['María', 'Pedro'] }
+];
+
+progresoEstudiantes = [
+  { nombre: 'Carlos', curso: 'Lógica Computacional', modulo: 'Intro', porcentaje: 85 },
+  { nombre: 'Ana', curso: 'Lógica Computacional', modulo: 'Condicionales', porcentaje: 60 },
+  { nombre: 'Pedro', curso: 'Pensamiento Algorítmico', modulo: 'Bucles', porcentaje: 40 },
+];
 
 }
