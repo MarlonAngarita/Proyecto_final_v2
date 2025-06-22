2dd7671a7c5058880fdc98cc5361aeaf94ef9dbd
Elaine <elaine85leal@gmail.com>
2025-06-22 17:29:22 -0500

    actualizacion dashboard-profesor

--- Frontend/src/app/pages/Profesor/dashboard-profesor/dashboard-profesor.ts
+++ Frontend/src/app/pages/Profesor/dashboard-profesor/dashboard-profesor.ts
@@ -7,21 +7,58 @@ import { FormsModule, NgForm } from '@angular/forms';
   selector: 'app-dashboard-profesor',
   imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './dashboard-profesor.html',
-  styleUrl: './dashboard-profesor.css'
+  styleUrl: './dashboard-profesor.css',
 })
 export class DashboardProfesor {
   nombreProfesor = 'Profe Sandra';
-avatarURL = '/assets/avatar-profesor.png';
+  avatarURL = 'https://api.dicebear.com/9.x/fun-emoji/svg';
 
-cursosAsignados = [
-  { id: 1, nombre: 'Lógica Computacional', estudiantes: ['Carlos', 'Ana', 'Luis'] },
-  { id: 2, nombre: 'Pensamiento Algorítmico', estudiantes: ['María', 'Pedro'] }
-];
+  cursosAsignados = [
+    {
+      id: 1,
+      nombre: 'Lógica Computacional',
+      estudiantes: ['Carlos', 'Ana', 'Luis'],
+    },
+    {
+      id: 2,
+      nombre: 'Pensamiento Algorítmico',
+      estudiantes: ['María', 'Pedro'],
+    },
+    {
+      id: 3,
+      nombre: 'Pensamiento Algorítmico',
+      estudiantes: ['María', 'Pedro'],
+    },
+    {
+      id: 3,
+      nombre: 'Pensamiento Algorítmico',
+      estudiantes: ['María', 'Pedro'],
+    },
+    {
+      id: 4,
+      nombre: 'Pensamiento Algorítmico',
+      estudiantes: ['María', 'Pedro'],
+    },
+  ];
 
-progresoEstudiantes = [
-  { nombre: 'Carlos', curso: 'Lógica Computacional', modulo: 'Intro', porcentaje: 85 },
-  { nombre: 'Ana', curso: 'Lógica Computacional', modulo: 'Condicionales', porcentaje: 60 },
-  { nombre: 'Pedro', curso: 'Pensamiento Algorítmico', modulo: 'Bucles', porcentaje: 40 },
-];
-
+  progresoEstudiantes = [
+    {
+      nombre: 'Carlos',
+      curso: 'Lógica Computacional',
+      modulo: 'Intro',
+      porcentaje: 85,
+    },
+    {
+      nombre: 'Ana',
+      curso: 'Lógica Computacional',
+      modulo: 'Condicionales',
+      porcentaje: 60,
+    },
+    {
+      nombre: 'Pedro',
+      curso: 'Pensamiento Algorítmico',
+      modulo: 'Bucles',
+      porcentaje: 40,
+    },
+  ];
 }
