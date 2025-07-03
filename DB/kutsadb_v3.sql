76b19de2177ba63cbb3ad3b06ebbdf976e5ed309
julay-alb <albarracin790@gmail.com>
2025-07-02 20:28:53 -0500

    setings

--- DB/kutsadb_v3.sql
+++ DB/kutsadb_v3.sql
@@ -161,7 +161,6 @@ INSERT INTO `modulos` (`id_modulo`, `nombre_modulo`, `contenido_modulo`, `id_cur
 INSERT INTO `quiz` (`id_quiz`, `pregunta`, `opcion_a`, `opcion_b`, `opcion_c`, `opcion_d`, `respuesta_correcta`, `id_modulo`) VALUES (1, 'Pregunta1', 'Opcion A', 'Opcion B', 'Opcion C', 'Opcion D', 'Opcion A', 1), (2, 'Pregunta2', 'Opcion A', 'Opcion B', 'Opcion C', 'Opcion D', 'Opcion B', 2);
 --
 -- Insert data into Usuarios
---
 INSERT INTO `usuarios` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `is_staff`, `is_active`, `date_joined`, `email`, `nombre`, `id_avatar`, `id_rol`, `id_tipo_documento`) VALUES (1, 'password1', '2023-01-01 00:00:00', true, 'usuario1', 'Nombre1', 'Apellido1', true, true, '2023-01-01 00:00:00', 'usuario1@example.com', 'Nombre Completo 1', 1, 1, 1), (2, 'password2', '2023-02-01 00:00:00', false, 'usuario2', 'Nombre2', 'Apellido2', false, true, '2023-02-01 00:00:00', 'usuario2@example.com', 'Nombre Completo 2', 2, 2, 2);
 --
 -- Insert data into RachasUsuario
